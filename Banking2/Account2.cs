00afbb8ad1a78bed0cbd720cf990b817d13ade33
max-student <max-student@maxtrain.com>
2021-02-15 21:27:13 -0500

    Add project files.

---
+++ Banking2/Account2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking {
+    class Account {
+        public string AccountNumber { get; set; }
+        public string RoutingNumber { get; set; }
+        public string Description { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; set; }
+
+        public void Deposit(decimal amount) {
+            if (amount >= 0) {
+                Balance += amount;
+            }
+        }
+        public void Withdrawal(decimal amount) {
+            if (amount <0) {
+                return;
+            }
+            if (Balance >= amount) {
+                Balance -= amount;
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }   } }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Banking2/Program2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Banking {
+    class Program2 {
+        static void Main(string[] args) {
+
+            var acct2 = new Account();
+            acct2.AccountNumber = "500.00";
+            Console.WriteLine($"Account {acct2.AccountNumber} balance is {acct2.Balance}");
+
+            acct2.Withdrawal(200);
+            Console.WriteLine($"Account {acct2.AccountNumber} balance is {acct2.Balance}");
+
+            acct2.Deposit(250);
+            Console.WriteLine($"Account {acct2.AccountNumber} balance is {acct2.Balance}");
+
+            acct2.Deposit(250);
+            Console.WriteLine($"Account {acct2.AccountNumber} balance is {acct2.Balance}");
+
+            acct2.Withdrawal(50);
+            Console.WriteLine($"Account {acct2.AccountNumber} balance is {acct2.Balance}");
+
+            acct2.Deposit(1000);
+            Console.WriteLine($"Account {acct2.AccountNumber} balance is {acct2.Balance}");
+
+            acct2.Withdrawal(500);
+            Console.WriteLine($"Account {acct2.AccountNumber} balance is {acct2.Balance}");
+
+            acct2.Deposit(550);
+            Console.WriteLine($"Account {acct2.AccountNumber} balance is {acct2.Balance}");
+
+            acct2.Withdrawal(50);
+            Console.WriteLine($"Account {acct2.AccountNumber} balance is {acct2.Balance}");
+
+
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
